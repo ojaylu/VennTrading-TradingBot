2d975fd62b38caed5602d52ba5ecd077eb6726d2
oscar <u3572091@connect.hku.hk>
2023-03-11 16:36:24 +0800

    first commit

---
+++ src/test/java/edu/hku/cs/fyp/venntrading/tradingbot/TradingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hku.cs.fyp.venntrading.tradingbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
